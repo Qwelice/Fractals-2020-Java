9037b033621799c6037dfb74e858c5b822eba4bf
Qwelice <kamilzalyalov00@gmail.com>
2020-12-18 01:46:32 +0300

    Обновленная версия

--- src/ru/smak/gui/graphics/video/mediaprocessor/managers/videomanager/PullListener.java
+++
@@ -1,7 +0,0 @@
-package ru.smak.gui.graphics.video.mediaprocessor.managers.videomanager;
-
-import ru.smak.gui.graphics.coordinates.CartesianScreenPlane;
-
-public interface PullListener {
-    void timeToPull(CartesianScreenPlane base);
-}
